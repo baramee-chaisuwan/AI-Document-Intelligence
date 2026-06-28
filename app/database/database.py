3f00f0e1fe6480117f87bdd6d35b3b1a80446c07
modernaxz <baramee.18chaisuwan@gmail.com>
2026-06-28 18:54:47 +0700

    feat: add alembic migration setup and env-based database config

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv 
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
