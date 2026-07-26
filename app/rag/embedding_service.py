e80a268b781c41ade5577ed486c59077be6e39fe
modernaxz <baramee.18chaisuwan@gmail.com>
2026-07-27 04:26:51 +0700

    lazy load sentence transformer embedding model

--- app/rag/embedding_service.py
+++ app/rag/embedding_service.py
@@ -1,17 +1,21 @@
-from sentence_transformers import SentenceTransformer
-
 model = None
+
 
 def get_model():
 
     global model
 
     if model is None:
+
+        from sentence_transformers import SentenceTransformer
+
         model = SentenceTransformer(
-            "all-MiniLM-L6-v2"
+            "all-MiniLM-L6-v2",
+            device="cpu"
         )
 
     return model
+
 
 def create_embedding(text):
 
